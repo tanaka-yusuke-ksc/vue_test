f945260d9751cd76c0837e55e7375f7a134224d4
田中 裕介 <tanaka-yusuke@ksc.co.jp>
2021-01-09 11:25:03 +0900

    axios停止

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
-import axios from 'axios'
+//import axios from 'axios'
 
 Vue.config.productionTip = false
 
