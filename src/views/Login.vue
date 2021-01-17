013ba2036caeecf7f454495d54f1850f62c3a9b4
田中 裕介 <tanaka-yusuke@ksc.co.jp>
2021-01-17 23:19:26 +0900

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-card width="400px" class="mx-auto mt-5">
       <v-card-title>
-        <h1 class="display-1">ログイン４</h1>
+        <h1 class="display-1">ログイン５</h1>
       </v-card-title>
       <v-card-text>
         <v-form>
